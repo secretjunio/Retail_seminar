a86feb2fe68bdaaca3665fdb56bac03342b656e0
secretjunio <vuvanthien0812@gmail.com>
2022-04-25 20:12:30 +0700

    migration

---
+++ API/Migrations/20220425130946_change_table_detail.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class change_table_detail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_checked",
+                table: "DeliveryOrderDetails");
+
+            migrationBuilder.RenameColumn(
+                name: "product_instance_id",
+                table: "DeliveryOrderDetails",
+                newName: "product_line_id");
+
+            migrationBuilder.AddColumn<int>(
+                name: "quantity",
+                table: "DeliveryOrderDetails",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "quantity",
+                table: "DeliveryOrderDetails");
+
+            migrationBuilder.RenameColumn(
+                name: "product_line_id",
+                table: "DeliveryOrderDetails",
+                newName: "product_instance_id");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_checked",
+                table: "DeliveryOrderDetails",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
